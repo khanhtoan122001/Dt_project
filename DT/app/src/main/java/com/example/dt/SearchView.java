25f59ea86f3b8ef39fa6b3e41fc0787c3d43de9b
khanhtoan122001 <71191923+khanhtoan122001@users.noreply.github.com>
2021-06-21 04:45:42 +0700

    123

--- DT/app/src/main/java/com/example/dt/SearchView.java
+++ DT/app/src/main/java/com/example/dt/SearchView.java
@@ -1,14 +1,26 @@
 package com.example.dt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SearchView extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_view);
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
     }
 }
+
